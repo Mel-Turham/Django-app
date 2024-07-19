82d10180530f1d62d7272b124ae86e585eb6f8b4
Mel/ <tchoikuemel06@gmail.com>
2024-07-19 16:59:22 +0100

    passing data from template

--- base/views.py
+++ base/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 
+rooms = [
+    {'id': 1, "name": 'Lets learn python'},
+    {'id': 2, "name": 'Design with me'},
+    {'id': 3, "name": 'Frontend developers'},
+]
+
 
 def home(request):
-    return render(request, 'home.html')
+    context = {'rooms': rooms}
+    return render(request, 'home.html', context)
 
 
 def room(request):
